36cb79927f251c79c54aa2660dc011cd4da562ad
zohaib <muhammadzohaibranjha42@gmail.com>
2024-11-04 00:40:34 -0800

    import inspirational quotes library

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 import axios from "axios";
+import Quote from "inspirational-quotes";
 
 axios.get("https://jsonplaceholder.typicode.com/todos/1").then(function (response) {
   // handle success
   console.log(response);
 });
+
+console.log(Quote.getQuote());
